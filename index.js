04acd5c43eb9e709de0a1d20465e1d7eed6d6d33
SerovaKaterina <k.serova.k@yandex.ru>
2023-06-25 00:39:55 +0300

    add js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const cities = [Москва, Мурманск, Краснодар, Иркутск, Хабаровск];
+const citiesData = document.querySelector('.data');
+const cities = ["Москва", "Мурманск", "Краснодар", "Иркутск", "Хабаровск"];
 const temp = [];
+
+for (let i = 0; i < cities.length; i++) {
+    const temperatura = prompt(`Введите температуру для ${cities[i]}`);
+    temp.push(temperatura);
+    const city = document.createElement('li');
+    city.textContent = `Температура в городе ${cities[i]}: ${temperatura} °С`;
+    citiesData.append(city);
+};
+
+
